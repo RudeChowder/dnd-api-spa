355892daef9fe33648a7d595c2ef9a40efedeaec
Stephen Crane <scrane88@gmail.com>
2021-06-11 14:43:08 -0400

    basic css and placeholder html card

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // GET all the monsters from the api
+  // GET the info for each monster
+  // create a monster-card for each monster
+  // fill in info into the correct elements
+  // attach the elements to the card
+  // attach the card to the monster container
+})
